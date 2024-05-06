426160fff214a9cd17c4f687a1180456a7312ce7
haleytolar <153100456+haleytolar@users.noreply.github.com>
2024-05-06 15:06:35 -0500

    add genres

--- src/components/search-bar/search-bar.js
+++ src/components/search-bar/search-bar.js
@@ -13,11 +13,14 @@ const SearchBar = ({ searchTerm, onSearch, selectedGenre, onGenreSelect }) => {
           <Dropdown.Item onClick={() => onGenreSelect("Action")} style={{ color: "red" }}>
             Action
           </Dropdown.Item>
-          <Dropdown.Item onClick={() => onGenreSelect("Comedy")} style={{ color: "red" }}>
-            Comedy
+          <Dropdown.Item onClick={() => onGenreSelect("Comedy-drama")} style={{ color: "red" }}>
+          Comedy-drama
           </Dropdown.Item>
           <Dropdown.Item onClick={() => onGenreSelect("Drama")} style={{ color: "red" }}>Drama</Dropdown.Item>
-          {/* Add more genres as needed */}
+          <Dropdown.Item onClick={() => onGenreSelect("Gangster")} style={{ color: "red" }}>Gangster</Dropdown.Item>
+          <Dropdown.Item onClick={() => onGenreSelect("Horror")} style={{ color: "red" }}>Horror</Dropdown.Item>
+          <Dropdown.Item onClick={() => onGenreSelect("Romance")} style={{ color: "red" }}>Romance</Dropdown.Item>
+          <Dropdown.Item onClick={() => onGenreSelect("Science fiction")} style={{ color: "red" }}>Science fiction</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     </div>
