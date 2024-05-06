58bccd6785a2d4daef91efca5e2fb995d526c4e6
haleytolar <153100456+haleytolar@users.noreply.github.com>
2024-05-06 10:21:34 -0500

    import browser router

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,19 @@
 import { createRoot } from "react-dom/client";
-
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { MainView } from "./components/main-view/main-view";
 import Container from "react-bootstrap/Container";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
 const App = () => {
   return (
-    <Container style={{ textAlign: "center", padding: "20px" }}>
-      <h1 style={{ marginBottom: "20px", fontSize: "4em", color: "white" }}>🄼🄾🅅🄸🄴🄵🄻🄸🅇</h1>
-      <MainView />
-    </Container>
+    <BrowserRouter>
+      <Container style={{ textAlign: "center", padding: "20px" }}>
+        <h1 style={{ marginBottom: "20px", fontSize: "4em", color: "white" }}>🄼🄾🅅🄸🄴🄵🄻🄸🅇</h1>
+        <MainView />
+      </Container>
+    </BrowserRouter>
   );
 };
 
